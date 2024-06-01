d481f918c8dfc4e6a7b06af7b0916484c6f4e787
CptShini <gabrielnoah.fabricius@gmail.com>
2024-06-01 19:15:14 +0200

    Improved position evaluator and added functionality for evaluating board positioning

--- Chess-Bot/src/My Bot/BestBot/BestBotV2/Evaluations/Evaluators/Positioning/PositionEvaluator.cs
+++ Chess-Bot/src/My Bot/BestBot/BestBotV2/Evaluations/Evaluators/Positioning/PositionEvaluator.cs
@@ -17,14 +17,38 @@ internal static class PositionEvaluator
 
     private static float EvaluateMoveValueboard(this Move move, bool whiteMadeMove)
     {
-        if (Valueboard.PieceValueboards.TryGetValue(move.MovePieceType, out var valueBoard))
-        {
-            int source = whiteMadeMove ? move.StartSquare.Index : 63 - move.StartSquare.Index;
-            int target = whiteMadeMove ? move.TargetSquare.Index : 63 - move.TargetSquare.Index;
+        PieceType pieceType = move.MovePieceType;
 
-            return (valueBoard[target] - valueBoard[source]) / 100f;
+        int start = move.StartSquare.PerspectiveIndex(whiteMadeMove);
+        int target = move.TargetSquare.PerspectiveIndex(whiteMadeMove);
+
+        float startPositionValue = EvaluatePiecePositioning(pieceType, start);
+        float targetPositionValue = EvaluatePiecePositioning(pieceType, target);
+
+        return targetPositionValue - startPositionValue;
+    }
+
+    internal static float EvaluateBoardPositioning(Board board)
+    {
+        float evaluation = 0f;
+
+        for (int i = 0; i < 64; i++)
+        {
+            Square iSquare = new Square(i);
+            Piece iPiece = board.GetPiece(iSquare);
+
+            bool whitePiece = iPiece.IsWhite;
+            int perspectiveIndex = iSquare.PerspectiveIndex(whitePiece);
+
+            float worth = EvaluatePiecePositioning(iPiece.PieceType, perspectiveIndex);
+
+            evaluation += whitePiece ? worth : -worth;
         }
 
-        return 0f;
+        return evaluation;
     }
+
+    private static float EvaluatePiecePositioning(PieceType pieceType, int positionIndex) => Valueboard.PieceValueboards.TryGetValue(pieceType, out var valueBoard) ? valueBoard[positionIndex] / 100f : 0f;
+
+    private static int PerspectiveIndex(this Square square, bool whitePerspective) => whitePerspective ? square.Index : 63 - square.Index;
 }
