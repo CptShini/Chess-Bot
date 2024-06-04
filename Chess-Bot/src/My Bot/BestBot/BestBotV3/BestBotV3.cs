d96bf06714bf24b953ad616e1291946fd61ba596
CptShini <gabrielnoah.fabricius@gmail.com>
2024-06-04 19:15:11 +0200

    Integrated Thinker and Searcher into V3

--- Chess-Bot/src/My Bot/BestBot/BestBotV3/BestBotV3.cs
+++ Chess-Bot/src/My Bot/BestBot/BestBotV3/BestBotV3.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Chess_Challenge.My_Bot.BestBot.BestBotV3.Evaluation;
+using Chess_Challenge.My_Bot.BestBot.BestBotV3.Thinking;
 using ChessChallenge.API;
 using Timer = ChessChallenge.API.Timer;
 
@@ -22,129 +24,24 @@ public class BestBotV3 : IChessBot
         Improvements:
             Memory optimization using GetLegalMovesNonAlloc()
             Think-Time:
-                Factor in current evaluation (whether we are ahead or not)
-                Potentially factor in enemy remaining time?
-                Base entire time algorithm off some endgame/progression coefficient
-                Algorithm for determining/just precalculating whether or not searching an extra layer would take too long
+                Make these a part of the equation:
+                    Current evaluation
+                    Opponent remaining time
+                    Endgame coefficient
+                Neural Network model for estimating/predicting think time
         Problems/Bugs:
             Professional repetition enjoyer
-            Still potentially some checkmate/draw bugs?
-            Promoting pawns to stuff other than queens?
-                Probably caused when it'll be checkmate either way?
     */
 
-    private static readonly Random Random = new();
-    
-    private BoardEvaluation _boardEvaluation;
-    private CancellationTokenSource _cts;
-
-    private const int ExpectedTurnCount = 70;
-    private const float SpeedUpTime = 4;
+    private Thinker _thinker;
     
     public Move Think(Board board, Timer timer)
     {
-        _cts = new();
-        _boardEvaluation = new(board);
+        _thinker = new(board, timer);
+
+        ScoredMove scoredMove = _thinker.IterativeDeepening();
+        Console.WriteLine(scoredMove);
         
-        return GetMove(board, timer);
-    }
-
-    private static TimeSpan CalculateThinkTime(Timer timer)
-    {
-        int startTime = timer.GameStartTimeMilliseconds;
-        int turnCountFraction = startTime / ExpectedTurnCount;
-
-        int remainingTime = timer.MillisecondsRemaining;
-        int remainingFraction = (int)(remainingTime / (ExpectedTurnCount / SpeedUpTime));
-
-        int timeThisTurn = Math.Min(turnCountFraction, remainingFraction);
-        return TimeSpan.FromMilliseconds(timeThisTurn);
-    }
-    
-    private Move GetMove(Board board, Timer timer)
-    {
-        Task<(int eval, Move move, int moveDepth)> task = Task.Run(() => IterativeDeepening(board));
-
-        TimeSpan thinkTime = CalculateThinkTime(timer);
-        if (!task.Wait(thinkTime)) _cts.Cancel();
-
-        Console.WriteLine($"{task.Result.move} | Move-Depth: {task.Result.moveDepth} | Eval: {task.Result.eval}");
-
-        return task.Result.move;
-    }
-    
-    private (int, Move, int) IterativeDeepening(Board board)
-    {
-        int bestEvaluation = 0;
-        Move bestMove = board.GetLegalMoves()[0];
-
-        int depth = 1;
-        for (;; depth++)
-        {
-            var (eval, move) = SearchForBestMove(board, depth);
-            
-            if (_cts.Token.IsCancellationRequested) break;
-            
-            bestEvaluation = eval;
-            bestMove = move;
-
-            if (bestEvaluation >= 9000) break;
-        }
-
-        return (bestEvaluation, bestMove, depth - 1);
-    }
-    
-    private (int, Move) SearchForBestMove(Board board, int maxDepth)
-    {
-        Move bestMove = Move.NullMove;
-        int bestEvaluation = int.MinValue;
-
-        Move[] legalMoves = board.GetLegalMoves();
-        foreach (Move move in legalMoves)
-        {
-            int moveEvaluation = _boardEvaluation.EvaluateMove(move, EvaluationFunction);
-            
-            if (_cts.Token.IsCancellationRequested) break;
-            
-            bool newMoveIsBetter = moveEvaluation > bestEvaluation;
-            bool sameMoveButFunnier = moveEvaluation == bestEvaluation && Random.Next(2) == 0;
-            if (!(newMoveIsBetter || sameMoveButFunnier)) continue;
-            
-            bestEvaluation = moveEvaluation;
-            bestMove = move;
-        }
-        
-        return (bestEvaluation, bestMove);
-        
-        int EvaluationFunction() => -Search(maxDepth);
-    }
-
-    private int Search(int depth, int alpha = -999999, int beta = 999999)
-    {
-        if (depth == 0)
-        {
-            bool gameHasEnded = _boardEvaluation.GameHasEnded(out int endEvaluation);
-            return gameHasEnded ? endEvaluation : SearchAllCaptures(alpha, beta);
-        }
-
-        if (_cts.Token.IsCancellationRequested) return alpha;
-
-        return _boardEvaluation.AlphaBetaEvaluateMoves(EvaluationFunction, ref alpha, beta);
-
-        int EvaluationFunction() => -Search(depth - 1, -beta, -alpha);
-    }
-
-    private int SearchAllCaptures(int alpha, int beta)
-    {
-        int evaluationCurrent = _boardEvaluation.Current;
-
-        if (evaluationCurrent >= beta) return beta;
-        if (evaluationCurrent > alpha) alpha = evaluationCurrent;
-
-        if (_cts.Token.IsCancellationRequested) return alpha;
-
-        return _boardEvaluation.AlphaBetaEvaluateMoves(EvaluationFunction, ref alpha, beta, true);
-
-        int EvaluationFunction() => -SearchAllCaptures(-beta, -alpha);
+        return scoredMove.Move;
     }
 }
