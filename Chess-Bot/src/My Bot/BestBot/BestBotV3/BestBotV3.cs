3c3c332ff7c027199671533b0333e49c3fa2d9f8
CptShini <gabrielnoah.fabricius@gmail.com>
2024-06-04 23:55:57 +0200

    using's and TODO stuff

--- Chess-Bot/src/My Bot/BestBot/BestBotV3/BestBotV3.cs
+++ Chess-Bot/src/My Bot/BestBot/BestBotV3/BestBotV3.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using Chess_Challenge.My_Bot.BestBot.BestBotV3.Evaluation;
 using Chess_Challenge.My_Bot.BestBot.BestBotV3.Thinking;
 using ChessChallenge.API;
@@ -13,24 +10,13 @@ public class BestBotV3 : IChessBot
 {
     /*
     TODO:
-        Non-bot Stuff:
-            Running several games at once?
         Features:
             Endgame play, (How to evaluate?):
                 King to King distance
                 Enemy king near edges
             Transposition Table
-            Move ordering (from Iterative Deepening)
-        Improvements:
-            Memory optimization using GetLegalMovesNonAlloc()
-            Think-Time:
-                Make these a part of the equation:
-                    Current evaluation
-                    Opponent remaining time
-                    Endgame coefficient
-                Neural Network model for estimating/predicting think time
         Problems/Bugs:
-            Professional repetition enjoyer
+            Potential checkmate/draw bug (Professional repetition enjoyer)
     */
 
     private Thinker _thinker;
