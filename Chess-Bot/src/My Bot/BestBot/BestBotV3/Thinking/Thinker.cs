af4ebae002483dd11c4e264f7e5c9441e3d82c95
CptShini <gabrielnoah.fabricius@gmail.com>
2024-06-05 22:42:37 +0200

    Removed Task functionality from Thinker/Iterative Deepening

--- Chess-Bot/src/My Bot/BestBot/BestBotV3/Thinking/Thinker.cs
+++ Chess-Bot/src/My Bot/BestBot/BestBotV3/Thinking/Thinker.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using Chess_Challenge.My_Bot.BestBot.BestBotV3.Evaluation;
 using ChessChallenge.API;
 
@@ -9,12 +8,9 @@ namespace Chess_Challenge.My_Bot.BestBot.BestBotV3.Thinking;
 internal class Thinker
 {
     private const int ExpectedTurnCount = 40;
-    private const int MinimumTurnCount = 100;
     private const int SpeedUpTime = 2;
     
-    private readonly long _minimumThinkTime;
     private readonly long _maximumThinkTime;
-    
     private readonly Searcher _searcher;
     private readonly Timer _timer;
 
@@ -23,8 +19,6 @@ internal class Thinker
         _searcher = new(board);
         _timer = timer;
         
-        _minimumThinkTime = timer.GameStartTimeMilliseconds / MinimumTurnCount;
-        
         int turnCountTime = timer.GameStartTimeMilliseconds / ExpectedTurnCount;
         int emergencyTime = (int)(timer.MillisecondsRemaining / (ExpectedTurnCount / (float)SpeedUpTime));
         _maximumThinkTime = Math.Min(turnCountTime, emergencyTime);
@@ -32,46 +26,34 @@ internal class Thinker
 
     internal ScoredMove IterativeDeepening()
     {
-        ScoredMove currentBest = Think(0, GetThinkTimeEstimate(), out long timeTaken);
+        ScoredMove currentBest = Think(0, out long timeTaken);
         
         for (int depth = 1; ; depth++)
         {
             long thinkTimeEstimate = GetThinkTimeEstimate(timeTaken);
-            if (EstimatedTimeTooSlow(thinkTimeEstimate)) break;
-            
-            try
-            {
-                currentBest = Think(depth, thinkTimeEstimate, out timeTaken);
-                if (currentBest.IsCheckmate) break;
-            }
-            catch (OperationCanceledException) { break; }
+            if (TimeToStopThinking(thinkTimeEstimate)) break;
+
+            currentBest = Think(depth, out timeTaken);
+            if (currentBest.IsCheckmate) break;
         }
         
         return currentBest;
     }
 
-    private long GetThinkTimeEstimate(long previousThinkTime = 0)
-    {
-        long estimateThinkTime = previousThinkTime * 7 + 30;
-        return Math.Max(_minimumThinkTime, estimateThinkTime);
-    }
-    
-    private bool EstimatedTimeTooSlow(long thinkTimeEstimate)
+    private static long GetThinkTimeEstimate(long previousThinkTime = 0) => previousThinkTime * 7 + 30;
+
+    private bool TimeToStopThinking(long thinkTimeEstimate)
     {
         long estimatedEndTime = _timer.MillisecondsElapsedThisTurn + thinkTimeEstimate;
         return estimatedEndTime > _maximumThinkTime;
     }
 
-    private ScoredMove Think(int maxDepth, long thinkTime, out long timeTaken)
+    private ScoredMove Think(int maxDepth, out long timeTaken)
     {
-        Task<ScoredMove> thinkTask = Task.Run(MoveSearcher);
-
         Stopwatch s = Stopwatch.StartNew();
-        if (!thinkTask.Wait(TimeSpan.FromMilliseconds(thinkTime))) throw new OperationCanceledException($"Ran out of time after thinking for {s.ElapsedMilliseconds}ms.");
-
+        ScoredMove result = _searcher.SearchForBestMove(maxDepth);
         timeTaken = s.ElapsedMilliseconds;
-        return thinkTask.Result;
-
-        ScoredMove MoveSearcher() => _searcher.SearchForBestMove(maxDepth);
+        
+        return result;
     }
 }
