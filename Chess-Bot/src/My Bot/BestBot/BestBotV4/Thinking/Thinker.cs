6c1b0d83738ac8655244520e6d54063459e17ac6
CptShini <gabrielnoah.fabricius@gmail.com>
2024-06-07 20:33:09 +0200

    T h o n k

--- Chess-Bot/src/My Bot/BestBot/BestBotV4/Thinking/Thinker.cs
+++ Chess-Bot/src/My Bot/BestBot/BestBotV4/Thinking/Thinker.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Chess_Challenge.My_Bot.BestBot.BestBotV4.Evaluation;
 using ChessChallenge.API;
 
@@ -25,28 +26,22 @@ internal class Thinker
 
     internal ScoredMove IterativeDeepening()
     {
-        int alpha = -Infinity;
-        int beta = Infinity;
-        
-        ScoredMove currentBest = Think(0, alpha, beta, GetThinkTimeEstimate(0), out long timeTaken);
+        ScoredMove currentBest = Think(out long timeTaken);
         for (int depth = 1; depth < DepthHardLimit; depth++)
         {
-            float thinkTimeEstimate = GetThinkTimeEstimate(depth, timeTaken);
+            long thinkTimeEstimate = GetThinkTimeEstimate(depth, timeTaken);
             if (TimeToStopThinking(thinkTimeEstimate)) break;
 
-            currentBest = Think(depth, alpha, beta, thinkTimeEstimate, out timeTaken);
+            currentBest = Think(out timeTaken, depth);
             if (currentBest.IsCheckmate) break;
         }
         
         return currentBest;
     }
     
-    private ScoredMove Think(int maxDepth, int alpha, int beta, float thinkTime, out long timeTaken)
+    private ScoredMove Think(out long timeTaken, int maxDepth = 0, int alpha = -Infinity, int beta = Infinity)
     {
         Stopwatch s = Stopwatch.StartNew();
-        
-        //Task<int> thinkTask = Task.Run(() => _searcher.Search(ref line, maxDepth, alpha, beta));
-        //if (!thinkTask.Wait(TimeSpan.FromMilliseconds(thinkTime * 1.5f))) throw new OperationCanceledException($"Time ran out while thinking after {s.ElapsedMilliseconds}ms.");
         
         Line line = new();
         int evaluation = _searcher.Search(ref line, maxDepth, alpha, beta);
@@ -56,41 +51,30 @@ internal class Thinker
         return thoughtProduct;
     }
     
-    private bool TimeToStopThinking(float thinkTimeEstimate)
+    private bool TimeToStopThinking(long thinkTimeEstimate)
     {
-        float estimatedEndTime = _timer.ElapsedTicks + thinkTimeEstimate;
+        long estimatedEndTime = _timer.ElapsedTicks + thinkTimeEstimate;
         return estimatedEndTime > _maximumThinkTime;
     }
     
-    private static float GetThinkTimeEstimate(int depth, long previousThinkTime = 0)
+    private static long GetThinkTimeEstimate(int depth, long previousThinkTime = 0)
     {
-        return depth switch
+        return (long)MathF.Max(0, EstimateThinkTime());
+
+        float EstimateThinkTime()
         {
-            0 => previousThinkTime + 225f,
-            1 => previousThinkTime * 3.7755f + 323.92f,
-            2 => previousThinkTime * 14.321f - 1762.3f,
-            3 => previousThinkTime * 8.6170f - 6272.1f,
-            4 => previousThinkTime * 11.293f - 102303f,
-            5 => previousThinkTime * 5.2390f + 232060f,
-            6 => previousThinkTime * 7.4013f + 972526f,
-            7 => previousThinkTime * 3.9426f + 20000000f,
-            _ => previousThinkTime * 7f
-        };
+            return depth switch
+            {
+                0 => previousThinkTime + 225f,
+                1 => previousThinkTime * 3.7755f + 323.92f,
+                2 => previousThinkTime * 14.321f - 1762.3f,
+                3 => previousThinkTime * 8.6170f - 6272.1f,
+                4 => previousThinkTime * 11.293f - 102303f,
+                5 => previousThinkTime * 5.2390f + 232060f,
+                6 => previousThinkTime * 7.4013f + 972526f,
+                7 => previousThinkTime * 3.9426f + 20000000f,
+                _ => previousThinkTime * 7f
+            };
+        }
     }
-    
-    /*private static float GetThinkTimeEstimate(int depth, long previousThinkTime = 0)
-    {
-        return depth switch
-        {
-            0 => previousThinkTime + 10f,
-            1 => previousThinkTime * 0.9902f + 2.0196f,
-            2 => previousThinkTime * 0.7300f + 5.5400f,
-            3 => previousThinkTime * 0.9394f + 12.532f,
-            4 => previousThinkTime * 4.3539f + 19.885f,
-            5 => previousThinkTime * 7.5979f + 23.850f,
-            6 => previousThinkTime * 6.2155f + 414.05f,
-            7 => previousThinkTime * 6.7331f - 91.745f,
-            _ => previousThinkTime * 7f
-        };
-    }*/
 }
